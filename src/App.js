46f4d6a4fd71728d8b7ed438123f2f51aba599dd
John Leintz <johnleintz@scaledflow.com>
2020-09-19 13:53:57 -0500

    add some test code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,73 @@
-import React from 'react';
+// https://www.youtube.com/watch?v=t2ypzz6gJm0
+// useRef does not cause a component to update when state is changed 
+// unlike useState - note, commented out version without useRef goes
+// into infinite loop
+
+import React, { useState, useEffect, useRef } from 'react'
 
 function App() {
+  const [name, setName] = useState('')
+  const renderCount = useRef(1)
+  console.log(renderCount);
+
+
+  useEffect(() => {
+   renderCount.current = renderCount.current + 5;
+  })
+  console.log(renderCount);
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <input value={name} onChange={e => setName(e.target.value)}/>
+    <div>My name is {name} </div>
+    <div>I rendered {renderCount.current} time</div>
+    </>
   );
 }
 
 export default App;
+
+// ---------------------------------------------------------
+// using useState as a counter will cause infinite loop
+
+// import React, { useState, useEffect } from 'react'
+
+// function App() {
+//   console.log('App');
+//   const [name, setName] = useState('test')
+//   const [renderCount, setRenderCount] = useState(0)
+
+//   console.log(name)
+//   useEffect(() => {
+//     console.log('useEffect');
+//     // setRenderCount(prevRenderCount => prevRenderCount + 1)
+//   })
+
+//   return (
+//     <>
+//     <input value={name} onChange={e => setName(e.target.value)}/>
+//     <div>My name is {name} </div>
+//     {console.log('return')}
+//     <div>I rendered {renderCount} time</div>
+//     </>
+
+    // <>
+    // <input value={name} onChange={e => setName(e.target.value)}/>
+    // <div>My name is {name} </div>
+    // {console.log('return')}
+    // <div>I rendered {renderCount} time</div>
+    // </>
+//   );
+// }
+
+// export default App;
+
+// order of execution
+// App.js:35 App
+// App.js:39 test
+// App.js:45 return
+// App.js:35 App
+// App.js:39 test
+// App.js:45 return
+// App.js:41 useEffect
+
